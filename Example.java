f9b8e40f50f0db65ee3298675e73f593c0172caa
dimitristm <115984529+dimitristm@users.noreply.github.com>
2026-08-11 10:27:50 +0300

    expand the example minijava file

--- Example.java
+++ Example.java
@@ -1,10 +1,48 @@
-class Main{
-    public static void main(String[] a){
+class Main {
+    public static void main(String[] a) {
+        System.out.println(new B().runTest(10));
     }
 }
 
-  class A extends Main{
-  }
+class A extends Main {
+    int myInt;
+    boolean myBool;
+    int[] myArray;
 
-  class B extends A{
-  }
+    public int init(int size) {
+        myInt = size;
+        myBool = true;
+        myArray = new int[size];
+        return 0;
+    }
+
+    public int getInt() {
+        return myInt;
+    }
+}
+
+class B extends A {
+    int anotherInt;
+
+    public int runTest(int size) {
+        int dummy;
+        int i;
+        dummy = this.init(size);
+        anotherInt = 0;
+        i = 0;
+
+        while (i < (myArray.length)) {
+            myArray[i] = i * 2;
+            anotherInt = anotherInt + (myArray[i]);
+            i = i + 1;
+        }
+
+        if (myBool && (0 < myInt)) {
+            System.out.println(anotherInt);
+        } else {
+            System.out.println(0);
+        }
+
+        return this.getInt();
+    }
+}
